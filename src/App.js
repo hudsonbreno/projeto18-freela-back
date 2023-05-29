c77ea9205fda7aed4b133d042fb103a2fc04717e
Breno <brennoohudson@gmail.com>
2023-05-29 17:19:22 -0300

    arquivos no src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import router from "../routes/index.routes.js"
+import router from "./routes/index.routes.js"
 import dotenv from "dotenv"
 
 const app = express()
